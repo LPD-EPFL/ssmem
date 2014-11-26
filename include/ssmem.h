886234ce0aa9db3800c88450bf9bc16688b6ae1e
Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
2014-11-26 14:56:59 +0100

    added MIT license

--- include/ssmem.h
+++ include/ssmem.h
@@ -1,3 +1,33 @@
+/*   
+ *   File: ssmem.h
+ *   Author: Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
+ *   Description: ssmem interface and structures
+ *   ssmem.h is part of ASCYLIB
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Vasileios Trigonakis <vasileios.trigonakis@epfl.ch>
+ *	      	      Distributed Programming Lab (LPD), EPFL
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
 /* December 10, 2013 */
 #ifndef _SSMEM_H_
 #define _SSMEM_H_
@@ -9,11 +39,7 @@
 /* parameters */
 /* **************************************************************************************** */
 
-#if defined(__sparc__)
-#  define SSMEM_GC_FREE_SET_SIZE 16379
-#else
-#  define SSMEM_GC_FREE_SET_SIZE 507
-#endif
+#define SSMEM_GC_FREE_SET_SIZE 507
 #define SSMEM_DEFAULT_MEM_SIZE (32 * 1024 * 1024L)
 
 /* **************************************************************************************** */
